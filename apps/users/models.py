39ac5ac2910680565d21d71487d2d018a3c1aad3
yingkun <925712087@qq.com>
2020-02-24 22:58:24 +0800

    配置xadmin的后台管理界面，包括用户，邮箱，轮播图

--- apps/users/models.py
+++ apps/users/models.py
@@ -19,19 +19,20 @@ class UserProfile(AbstractUser):
         verbose_name = '用户信息'
         verbose_name_plural = verbose_name
 
-    def __unicode__(self):
-        return self.username
-
 
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=30, verbose_name=u'验证码')
     email = models.EmailField(max_length=50, verbose_name=u'邮箱')
-    send_type = models.CharField(choices=(('register', u'注册'), ('forget', u'找回密码')), max_length=10)
-    send_time = models.DateTimeField(default=datetime.now)
+    send_type = models.CharField(choices=(('register', u'注册'), ('forget', u'找回密码')), max_length=10,
+                                 verbose_name=u'验证码类型')
+    send_time = models.DateTimeField(default=datetime.now, verbose_name=u'验证时间')
 
     class Meta:
         verbose_name = u'邮箱验证码'
         verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return '{0}({1})'.format(self.code, self.email)
 
 
 class Banner(models.Model):
@@ -44,4 +45,3 @@ class Banner(models.Model):
     class Meta:
         verbose_name = u'轮播图'
         verbose_name_plural = verbose_name
-
