be3602f0932065f95dd3214b41eb11026368e35e
yingkun <925712087@qq.com>
2020-02-28 15:00:25 +0800

    注册界面搭建

--- HYSFOnline/urls.py
+++ HYSFOnline/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
 
-from users.views import LoginView
+from users.views import LoginView, RegisterView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url('^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url('^login/$', LoginView.as_view(), name='login')
+    url('^login/$', LoginView.as_view(), name='login'),
+    url('^register/$', RegisterView.as_view(), name='register')
+
 ]
