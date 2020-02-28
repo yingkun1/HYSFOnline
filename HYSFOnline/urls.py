8c546c41cea58766cdcbf98b18f9e14db803f5a2
yingkun <925712087@qq.com>
2020-02-28 12:11:43 +0800

    不再使用视图方法，改写为视图类的形式

--- HYSFOnline/urls.py
+++ HYSFOnline/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
 
-from users.views import user_login
+from users.views import LoginView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url('^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url('^login/$', user_login, name='login')
+    url('^login/$', LoginView.as_view(), name='login')
 ]
---
+++ apps/users/forms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2020/2/28 12:09
+@Author  : yingkun
+@File    : forms.py
+@Software: PyCharm
+@contact: 925712087@qq.com
+@desc:
+"""
