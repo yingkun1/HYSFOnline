8d630c04fd47602bb03558fbba43c400e6d804f5
yingkun <925712087@qq.com>
2020-02-29 17:53:12 +0800

    邮件服务器配置1

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.hashers import make_password
-from .models import UserProfile
+from .models import UserProfile, EmailVerifyRecord
 from .forms import LoginForm, RegisterForm
 from django.db.models import Q
 from django.views.generic.base import View
@@ -32,8 +32,11 @@ class LoginView(View):
             pass_word = request.POST.get('password', '')
             user = authenticate(username=user_name, password=pass_word)
             if user is not None:
-                login(request, user)
-                return render(request, 'index.html')
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'index.html')
+                else:
+                    return render(request, 'login.html', {'msg': '未激活，请在邮箱中点击链接激活'})
             else:
                 return render(request, 'login.html', {'msg': '用户名或者密码错误，请重新登录'})
         else:
@@ -48,16 +51,33 @@ class RegisterView(View):
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
             user_name = request.POST.get('email','')
+            if UserProfile.objects.filter(email=user_name):
+                return render(request, 'register.html', {'msg':u'用户已经被注册了','register_form':register_form})
             pass_word = request.POST.get('password','')
             user_profile = UserProfile()
             user_profile.username = user_name   #这是邮箱
             user_profile.email = user_name
             user_profile.password = make_password(pass_word)
+            user_profile.is_active = False
             user_profile.save()
             send_register_email(user_name, 'register')
             return render(request, 'login.html')
         else:
-            return render(request, 'register.html')
+            return render(request, 'register.html', {'register_form':register_form})
+
+class ActiveUserView(View):
+    print u'进入到当前页面了'
+    def get(self,request,active_code):
+          all_records =  EmailVerifyRecord.objects.filter(code=active_code)
+          if all_records:
+              for record in all_records:
+                  email = record.email
+                  all_users = UserProfile.objects.filter(email=email)
+                  for user in all_users:
+                      user.is_active = True
+                      user.save()
+          return render(request, 'login.html')
+
 
 
 
