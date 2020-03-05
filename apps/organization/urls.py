99973bec6fda0278f2b2808f8204574e610e46da
yingkun <925712087@qq.com>
2020-03-05 19:16:55 +0800

    课程机构子页面

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -9,10 +9,11 @@
 @desc:
 """
 from django.conf.urls import url,include
-from .views import OrgView, AddUserAskView
+from .views import OrgView, AddUserAskView,OrgHomeView
 
 urlpatterns = [
     # 课程机构列表页
     url(r'^list/$', OrgView.as_view(), name='org_list'),
-    url(r'^add_ask',AddUserAskView.as_view(), name='add_ask')
+    url(r'^add_ask/$',AddUserAskView.as_view(), name='add_ask'),
+    url(r'^home/(?P<org_id>\d+)/$',OrgHomeView.as_view(),name='org_home')
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from .models import CourseOrg, CityDict
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from .forms import UserAskForm
+from courses.models import Course
 # Create your views here.
 
 class OrgView(View):
@@ -61,5 +62,14 @@ class AddUserAskView(View):
             return HttpResponse("{'status':'fail', 'msg':'添加出错'}",content_type='application/json')
 
 
+class OrgHomeView(View):
+    def get(self,request, org_id):
+        course_org = CourseOrg.objects.get(id =int(org_id))
+        all_courses = course_org.course_set.all()[:3]
+        all_teachers = course_org.teacher_set.all()[:1]
+        return render(request,'org-detail-homepage.html',{
+            'all_courses':all_courses,
+            'all_teachers':all_teachers
+        })
 
 
