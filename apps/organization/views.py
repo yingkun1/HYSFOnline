6848e545cd3d659c7a01a51e5d0007427e5c8935
yingkun <925712087@qq.com>
2020-03-05 15:12:44 +0800

    课程机构排序

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -8,8 +8,8 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 class OrgView(View):
     def get(self,request):
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
         all_citys = CityDict.objects.all()
+        hot_orgs = all_orgs.order_by('-click_nums')[:6]
         # 城市筛选
         city_id = request.GET.get('city', '')
         if city_id:
@@ -19,6 +19,13 @@ class OrgView(View):
         if category:
             all_orgs = all_orgs.filter(category = category)
 
+        sort = request.GET.get('sort')
+        if sort:
+            if sort == 'students':
+                all_orgs = all_orgs.order_by('-students')[:5]
+            elif sort == 'courses':
+                all_orgs = all_orgs.order_by('-course_nums')[:5]
+        org_nums = all_orgs.count()
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
@@ -33,5 +40,7 @@ class OrgView(View):
             'all_citys':all_citys,
             'org_nums':org_nums,
             'city_id':city_id,
-            'category':category
+            'category':category,
+            'hot_orgs':hot_orgs,
+            'sort':sort
         })
