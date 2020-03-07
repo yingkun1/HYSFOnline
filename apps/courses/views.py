0917e56180eb958c58db0968489f09244fc682fb
yingkun <925712087@qq.com>
2020-03-07 14:18:42 +0800

    课程页面列表页、分页、筛选

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,6 +1,9 @@
 # *_* coding:utf-8 *_*
 from django.shortcuts import render
 from django.views.generic.base import View
+from .models import Course
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 # Create your views here.
@@ -8,5 +11,27 @@ from django.views.generic.base import View
 
 class CourseListView(View):
     def get(self,request):
-        return render(request, 'course-list.html',{})
+        all_courses = Course.objects.all().order_by("-add_time")
+        hot_courses = Course.objects.all().order_by("-click_nums")[:3]
+        sort = request.GET.get('sort')
+        # 课程排序
+        if sort:
+            if sort == 'students':
+                all_courses = all_courses.order_by('-students')
+            elif sort == 'hor':
+                all_courses = all_courses.order_by('-click_nums')
+        # 对课程进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_courses, 3, request=request)
+
+        courses = p.page(page)
+        return render(request, 'course-list.html',{
+            'all_courses':courses,
+            'sort':sort,
+            'hot_courses':hot_courses
+        })
 
