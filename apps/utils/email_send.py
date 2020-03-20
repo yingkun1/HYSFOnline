fa717974d2aef6f66461544482d1c407b7e10e69
yingkun <925712087@qq.com>
2020-03-20 17:47:40 +0800

    切换邮箱

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -26,7 +26,7 @@ def send_register_email(email,send_type='register'):
 
     if send_type == 'register':
         email_title = u'衡师在线网注册激活链接'
-        email_body = u'请点击下面的的链接激活你的账号:http://127.0.0.1:8000/active/{0}'.format(email_record.code)
+        email_body = u'请点击下面的的链接激活你的账号:http://101.132.138.215/active/{0}'.format(email_record.code)
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         print send_status
         print u'发送邮件成功'
@@ -35,7 +35,7 @@ def send_register_email(email,send_type='register'):
 
     if send_type == 'forget':
         email_title = u'衡师在线网密码重置链接'
-        email_body = u'请点击下面的的链接重置你的密码:http://127.0.0.1:8000/reset/{0}'.format(email_record.code)
+        email_body = u'请点击下面的的链接重置你的密码:http://101.132.138.215/reset/{0}'.format(email_record.code)
         print u'尝试发送邮件'
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         print send_status
